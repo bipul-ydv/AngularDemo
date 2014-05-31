aafb8956bf7dc31b3c8400728514b5e369d7107a
Manish Bharti <manish@middleware360.com>
2014-06-01 00:50:11 +0530

    Updated BootStrap Data and Added testarea for description field

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,9 +3,10 @@ import angulardemo.Product
 class BootStrap {
 
     def init = { servletContext ->
-        5.times {
-            new Product(name: "Product${it + 1}", category: "Category${it % 2 ? 1 : 2}", serialNumber: "100${it}", price: 1000 * (it + 1), description: "Description ${it + 1}").save(failOnError: true, flush: true)
-        }
+        new Product(name: "BlackBerry Z30", category: "Mobiles", serialNumber: "AB23", price: 34400,
+                description: "Document Viewer, Document Editor, Pushmail (Microsoft Exchange ActiveSync), Documents To Go, Adobe Reader, Microsoft Word, Microsoft Excel, Microsoft PowerPoint, Print To Go, eBook, Spreadsheets").save(failOnError: true, flush: true)
+        new Product(name: "Lenovo S930", category: "Mobiles", serialNumber: "AM47", price: 19200,
+                description: "Auto Focus, High Speed Bursts of Upto 99 In-a-row, Panoramic Scene, HDR, Intelligent Smile Correction, Multiple Angle View, Self-timer").save(failOnError: true, flush: true)
     }
 
     def destroy = {
